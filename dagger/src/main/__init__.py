af43d9dbc06fbf09db27908086a8d758811a1d7b
Cornelius Schuchardt <cornelius.schuchardt@beck.de>
2024-04-12 13:20:30 +0000

    first commit

---
+++ dagger/src/main/__init__.py
@@ -0,0 +1,43 @@
+import dagger
+from dagger import dag, function, object_type
+
+
+@object_type
+class TerraformModule:
+
+    @function
+    async def terraform_plan(self, directory_arg: dagger.Directory, azauth_directory: dagger.Directory)-> str:
+        return await (
+            dag.container()
+            .from_("devopsinfra/docker-terragrunt:azure-tf-1.7.0-tg-0.54.21")
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_mounted_directory("/root/.azure", azauth_directory)
+            .with_workdir("/mnt/test")
+            .with_exec(["terraform", "init"])
+            .with_exec(["terraform", "plan"])
+            .stdout()
+        )
+
+    @function
+    async def terraform_checkov(self, directory_arg: dagger.Directory)-> str:
+        return await (
+            dag.container()
+            .from_("bridgecrew/checkov:latest")
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_workdir("/mnt/test")
+            .with_exec(["-d", ".","--quiet","--compact" ])
+            .stdout()
+        )
+    
+    @function
+    async def terraform_tflint(self, directory_arg: dagger.Directory)-> str:
+        return await (
+            dag.container()
+            .from_("ghcr.io/terraform-linters/tflint")
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_workdir("/mnt/test")
+            # .with_exec(["-d", ".","--quiet","--compact" ])
+            .stdout()
+        )
+    
+    
