6989299c93f11eb6852c5a70e1ac1c8070f68824
Cornelius Schuchardt <cornelius.schuchardt@beck.de>
2024-04-16 08:25:53 +0000

    minor

--- dagger/src/main/__init__.py
+++ dagger/src/main/__init__.py
@@ -1,3 +1,4 @@
+import requests
 import dagger
 from dagger import dag, function, object_type
 
@@ -42,6 +43,7 @@ class TerraformModule:
     
     @function
     async def terragrunt_test(self, directory_arg: dagger.Directory,azauth_directory: dagger.Directory)-> dagger.Container:
+        azurermProviderVersions = ["3.98.00", "2.47.0"]
         return await (
             dag.container()
             .from_("devopsinfra/docker-terragrunt:azure-tf-1.8.0-tg-0.57.0")
@@ -54,16 +56,49 @@ class TerraformModule:
             # .with_exec(["go","test"])
             # .stdout()
         )
-    # @function
-    # async def terragrunt_test(self, directory_arg: dagger.Directory,azauth_directory: dagger.Directory)-> str:
-    #     return await (
-    #         dag.container()
-    #         .from_("devopsinfra/docker-terragrunt:azure-tf-1.8.0-tg-0.57.0")
-    #         .with_mounted_directory("/mnt", directory_arg)
-    #         .with_mounted_directory("/root/.azure", azauth_directory)
-    #         .with_workdir("/mnt/tg_test")
-    #         .with_exec(["go","mod","init","terratest"])
-    #         .with_exec(["go","mod","tidy"])
-    #         .with_exec(["go","test"])
-    #         .stdout()
-    #     )
+        
+    @function
+    def get_latest_provider_version(provider_name):
+        # Construct the URL for the provider on the Terraform Registry
+        url = f"https://registry.terraform.io/v1/providers/{provider_name}/versions"
+
+        try:
+            # Send a GET request to the Terraform Registry
+            response = requests.get(url)
+
+            # Check if the request was successful
+            if response.status_code == 200:
+                # Parse the JSON response
+                data = response.json()
+                # Sort versions in descending order
+                sorted_versions = sorted(data['versions'], key=lambda x: x['version'], reverse=True)
+                # Extract the latest version from the response
+                latest_version = sorted_versions[0]['version']
+                return latest_version
+            else:
+                # If the request was not successful, print an error message
+                print(f"Error: Unable to fetch provider version. Status code: {response.status_code}")
+                return None
+        except Exception as e:
+            # If an exception occurs during the request, print an error message
+            print(f"Error: {e}")
+            return None
+
+    
+    @function
+    async def terragrunt_test(self, directory_arg: dagger.Directory,azauth_directory: dagger.Directory, azure_rm_provider_version="latest")-> str:
+        if azure_rm_provider_version == "latest":
+            # Use latest Provider Version
+            azure_rm_provider_version = get_latest_provider_version("hashicorp/azurerm")
+        return await (
+            dag.container()
+            .from_("devopsinfra/docker-terragrunt:azure-tf-1.8.0-tg-0.57.0")
+            .with_env_variable("TG_AZURERM_PROVIDER_VERSION",azure_rm_provider_version)
+            .with_mounted_directory("/mnt", directory_arg)
+            .with_mounted_directory("/root/.azure", azauth_directory)
+            .with_workdir("/mnt/tg_test")
+            .with_exec(["go","mod","init","terratest"])
+            .with_exec(["go","mod","tidy"])
+            .with_exec(["go","test"])
+            .stdout()
+        )
